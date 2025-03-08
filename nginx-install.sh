def16318d4625bea4cee5f411ab962700fe258dc
Your Dmitri Donskoy <crooper22@gmail.com>
2025-03-08 10:11:16 +0000

    update 12:11-shabbat

--- nginx-install.sh
+++ nginx-install.sh
@@ -4,7 +4,7 @@
 #  _   _  _____ _____ _   ___   __  _______ ____   ____  _      
 # | \ | |/ ____|_   _| \ | \ \ / / |__   __/ __ \ / __ \| |     
 # |  \| | |  __  | | |  \| |\ V /     | | | |  | | |  | | |     
-# | . ` | | |_ | | | | . ` | > <      | | | |  | | |  | | |     
+# | . ` | | |_ | | | . ` | > <      | | | | |  | | |  | | |     
 # | |\  | |__| |_| |_| |\  |/ . \     | | | |__| | |__| | |____ 
 # |_| \_|\_____|_____|_| \_/_/ \_\    |_|  \____/ \____/|______|
 
@@ -25,73 +25,74 @@ set -o pipefail
 USER_HOME_DIR=$(eval echo ~$SUDO_USER)  
 # Get the username of the current user who run sudo .
 USERNAME=$(basename $USER_HOME_DIR)
-## Log file
-LOGFILE=/var/log/nginx_install/nginx_install.log
+
 #######################################################################
+
+# Create LOGS directory if  not exist
+if [ ! -d "/var/log/nginx-tool" ]; then
+    echo "LOGS directory does not exist. Creating LOGS directory..."
+    mkdir -p /var/log/nginx-tool
+fi
+
+## Log file
+LOGFILE=/var/log/nginx-tool/nginx_tool.log
+
 
 # Check if user is root
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
-    exit
+    echo "Please run as root" 
 fi
-
-# Create LOGS directory if  not exist
-if [ ! -d "/var/log/nginx_install" ]; then
-    echo "LOGS directory does not exist. Creating LOGS directory..."
-    mkdir -p /var/log/nginx_install
-fi
-
 
 
 # Install nginx service
 function install_nginx(){
   if [ -x "$(command -v nginx)" ]; then
-      echo "Nginx already installed. Skipping installation..."
+      echo "Nginx already installed. Skipping installation..." | tee -a $LOGFILE
   else
-      echo "Installing nginx..."
-      sudo apt update && apt install nginx -y
-      echo "Nginx installed!"
+      echo "Installing nginx..." | tee -a $LOGFILE
+      sudo apt update && apt install nginx -y | tee -a $LOGFILE
+      echo "Nginx installed!" | tee -a $LOGFILE
   fi
 } 
 
 # Remove nginx service
 function remove_nginx(){
   if [ -x "$(command -v nginx)" ]; then
-      echo "Nginx installed. Uninstalling nginx service..."
-      sudo apt remove nginx --purge -y
-      rm -rf /etc/nginx/sites-available/*
-      rm -rf /etc/nginx/sites-enabled/*
-      rm -rf /var/www/*
-      rm -rf /etc/nginx/.htpasswd
-      rm -rf /var/log/nginx_install
-      rm -rf $USER_HOME_DIR/public_html/index.html
-      echo "Nginx uninstalled!"
+      echo "Nginx installed. Uninstalling nginx service..." | tee -a $LOGFILE
+      sudo apt remove nginx --purge -y | tee -a $LOGFILE
+      rm -rf /etc/nginx/sites-available/* | tee -a $LOGFILE
+      rm -rf /etc/nginx/sites-enabled/* | tee -a $LOGFILE
+      rm -rf /var/www/* | tee -a $LOGFILE
+      rm -rf /etc/nginx/.htpasswd | tee -a $LOGFILE
+      rm -rf /var/log/nginx-tool | tee -a $LOGFILE
+      rm -rf $USER_HOME_DIR/public_html/index.html | tee -a $LOGFILE
+      echo "Nginx uninstalled!" | tee -a $LOGFILE
   fi
 } 
-
 
 
 ######################################################################
 ### create a new virtual host with basic params
 function create_virtual_host(){
+    ### get the domain name
     read -p "Enter the domain name for the new virtual host : " domain_name
 
     ### check if virtual host already exists
     if [ -e /etc/nginx/sites-enabled/$domain_name ]; then
-        echo "Virtual host already exists. Exiting..."
+        echo "Virtual host already exists. Exiting..." | tee -a $LOGFILE
         exit 1
     fi
 
-    echo "Creating new virtual host...".
+    echo "Creating new virtual host..." | tee -a $LOGFILE
 
     ### create site-directory
-    mkdir -p /var/www/$domain_name
-    sudo chown -R $USER:$USER /var/www/$domain_name
-    sudo chmod -R 755 /var/www/$domain_name
+    mkdir -p /var/www/$domain_name | tee -a $LOGFILE
+    sudo chown -R $USERNAME:$USERNAME /var/www/$domain_name | tee -a $LOGFILE
+    sudo chmod -R 755 /var/www/$domain_name | tee -a $LOGFILE
 
     ### create sample index.html
     cat > /var/www/$domain_name/index.html <<EOF
-    <H1>Welcome to nginx new virtual host</H1> 
+    <H1>Welcome to $domain_name new virtual host</H1> 
 EOF
 
     ### create a new virtual host from template
@@ -113,33 +114,33 @@ EOF
 
     ### remove default virtual host from sites-enabled if exists
     if [ -e /etc/nginx/sites-enabled/default ]; then
-        sudo rm /etc/nginx/sites-enabled/default
+        sudo rm /etc/nginx/sites-enabled/default | tee -a $LOGFILE
     fi
 
     ### check if virtual host already exists , if exist then exit
     if [ -e /etc/nginx/sites-enabled/$domain_name ]; then
-        echo "Virtual host already exists. Exiting..."
+        echo "Virtual host already exists. Exiting..." | tee -a $LOGFILE
         exit 1
     fi
 
     ### Create link to the site directory and set permissions
-    ln -s /etc/nginx/sites-available/$domain_name /etc/nginx/sites-enabled/
-    chown -R $USER:$USER /etc/nginx/sites-available/$domain_name
-    chmod -R 755 /etc/nginx/sites-available/$domain_name
+    ln -s /etc/nginx/sites-available/$domain_name /etc/nginx/sites-enabled/ | tee -a $LOGFILE
+    chown -R $USERNAME:$USERNAME /etc/nginx/sites-available/$domain_name | tee -a $LOGFILE
+    chmod -R 755 /etc/nginx/sites-available/$domain_name | tee -a $LOGFILE
 
 
     ### Add new virtual host to /etc/hosts
-    echo "127.0.0.1 $domain_name" >> /etc/hosts
+    echo "127.0.0.1 $domain_name" >> /etc/hosts | tee -a $LOGFILE
 
     ### restart nginx
-    systemctl restart nginx
+    systemctl restart nginx | tee -a $LOGFILE
 
-    echo "Virtual host created!"
-    echo "You can access your website at http://$domain_name"
+    echo "Virtual host created!" | tee -a $LOGFILE
+    echo "You can access your website at http://$domain_name" | tee -a $LOGFILE
 
     ### Test with curl
-    echo "Testing the virtual host with curl..."
-    curl  http://$domain_name
+    echo "Testing the virtual host with curl..." | tee -a $LOGFILE
+    curl http://$domain_name | tee -a $LOGFILE
 
 }
 
@@ -148,21 +149,21 @@ function enable_user_dir() {
     read -p "Enter the domain name for the virtual host to enable user directory : " domain_name
 
     if [ ! -e "/etc/nginx/sites-enabled/$domain_name" ]; then
-        echo "Virtual host does not exist. Please create a virtual host first."
+        echo "Virtual host does not exist. Please create a virtual host first." | tee -a $LOGFILE
         exit 1    
     fi
 
     # Create user directory if it does not exist
-    mkdir -p "$USER_HOME_DIR/public_html"
+    mkdir -p "$USER_HOME_DIR/public_html" | tee -a $LOGFILE
 
     # Create a sample index.html
     cat > "$USER_HOME_DIR/public_html/index.html" <<EOF
     <H1>Welcome to nginx user_dir test server !</H1> 
 EOF
     ## Set permissions
-    sudo chown -R "$USERNAME:$USERNAME" "$USER_HOME_DIR/public_html"
-    sudo chmod 755 $USER_HOME_DIR/public_html
-    sudo chmod 644 $USER_HOME_DIR/public_html/index.html
+    sudo chown -R "$USERNAME:$USERNAME" "$USER_HOME_DIR/public_html" | tee -a $LOGFILE
+    sudo chmod 755 $USER_HOME_DIR/public_html | tee -a $LOGFILE
+    sudo chmod 644 $USER_HOME_DIR/public_html/index.html | tee -a $LOGFILE
 
 
     # Add user_dir configuration to Nginx
@@ -170,106 +171,106 @@ EOF
         location ~ ^/~(.+?)(/.*)?$ {\n\
             alias /home/\$1/public_html/$2;\n\
             index index.html index.htm;\n\
-        }' "/etc/nginx/sites-available/$domain_name"
+        }' "/etc/nginx/sites-available/$domain_name" | tee -a $LOGFILE
 
     # Restart Nginx
-    systemctl restart nginx
+    systemctl restart nginx | tee -a $LOGFILE
 
-    echo "Virtual host configured!"
-    echo "You can access your website at http://$domain_name/~$USERNAME"
+    echo "Virtual host configured!" | tee -a $LOGFILE
+    echo "You can access your website at http://$domain_name/~$USERNAME" | tee -a $LOGFILE
 
     ### Test with curl
-    echo "Testing the virtual host with curl..."
-    curl http://$domain_name/~$USERNAME/
+    echo "Testing the virtual host user_dir with curl..." | tee -a $LOGFILE
+    curl http://$domain_name/~$USERNAME/ | tee -a $LOGFILE
 }
 
 
 ##################  BASIC AUTHENTICATION  ####################################
 function enable_basic_auth() {
-    echo "Setting up basic authentication..."
-    sudo apt install -y apache2-utils nginx-extras
+    echo "Setting up basic authentication..." | tee -a $LOGFILE
+    sudo apt install -y apache2-utils nginx-extras | tee -a $LOGFILE
 
     read -p "Enter the domain name for the virtual host to enable basic auth : " domain_name
 
     ### check if virtual host already exists
     if [ ! -e "/etc/nginx/sites-enabled/$domain_name" ]; then
-        echo "Virtual host does not exist. Please create a virtual host first."
+        echo "Virtual host does not exist. Please create a virtual host first." | tee -a $LOGFILE
         exit 1    
     fi
 
     ## create a password file
     echo "Enter the username :"
     read username
-    htpasswd -c /etc/nginx/.htpasswd $username
+    htpasswd -c /etc/nginx/.htpasswd $username | tee -a $LOGFILE
 
     ### add basic auth to the virtual host
     sudo sed -i '13i\
         location /secure {\n\
             auth_basic "Restricted Area";\n\
             auth_basic_user_file /etc/nginx/.htpasswd;\n\
-          }' /etc/nginx/sites-available/$domain_name
+          }' /etc/nginx/sites-available/$domain_name | tee -a $LOGFILE
 
     ### restart nginx
-    systemctl restart nginx
+    systemctl restart nginx | tee -a $LOGFILE
     
-    echo "Basic authentication enabled !"
-    echo "You can access your secure website at http://$domain_name/secure"
+    echo "Basic authentication enabled !" | tee -a $LOGFILE
+    echo "You can access your secure website at http://$domain_name/secure" | tee -a $LOGFILE
 
     ### Test with curl
-    echo "Testing the virtual host with curl..."
+    echo "Testing the virtual host with curl..." | tee -a $LOGFILE
     read -p "Enter the username : " auth_user
     read -p "Enter the password : " auth_pass
-    curl -u auth_user:auth_pass http://$domain_name/secure/
+    curl -u auth_user:auth_pass http://$domain_name/secure/ | tee -a $LOGFILE
 
   }
 
 ##################  PAM AUTHENTICATION  ####################################
 function enable_auth_pam() {
-    echo "Setting up PAM authentication..."
-    sudo apt install -y libpam0g-dev libpam-modules
+    echo "Setting up PAM authentication..." | tee -a $LOGFILE
+    sudo apt install -y libpam0g-dev libpam-modules | tee -a $LOGFILE
+
+    read -p "Enter the domain name for the virtual host to enable PAM auth : " domain_name
+
+    ### check if virtual host already exists
+    if [ ! -e "/etc/nginx/sites-enabled/$domain_name" ]; then
+        echo "Virtual host does not exist. Please create a virtual host first." | tee -a $LOGFILE
+        exit 1    
+    fi
 
     ### add pam auth to the virtual host
     sudo sed -i '13i\
         location /auth-pam {\n\
             auth_pam "PAM Authentication";\n\
             auth_pam_service_name "nginx";\n\
-          }' /etc/nginx/sites-available/$domain_name
+          }' /etc/nginx/sites-available/$domain_name | tee -a $LOGFILE
 
 
     # Define the PAM configuration file for nginx
     PAM_FILE="/etc/pam.d/nginx"
-    NGINX_GROUP="www-data"
-    HTML_DIR="/var/www/html/$domain_name"
-    HTML_FILE="$HTML_DIR/index.html"
 
     # Append PAM configuration to nginx PAM file
     if ! grep -q "auth include common-auth" "$PAM_FILE"; then
-        echo -e "auth include common-auth\naccount include common-account" | sudo tee -a "$PAM_FILE"
+        echo -e "auth include common-auth\naccount include common-account" | sudo tee -a "$PAM_FILE" | tee -a $LOGFILE
     fi
 
     # Add nginx user to shadow group
-    sudo usermod -aG shadow "$NGINX_GROUP"
+    sudo usermod -aG shadow www-data | tee -a $LOGFILE
 
     # Reload nginx service
-    sudo systemctl reload nginx
-
-    # Create test directory and index.html file
-    sudo mkdir -p "$HTML_DIR"
-    echo "<html>
-    <body>
-    <div style='width: 100%; font-size: 40px; font-weight: bold; text-align: center;'>
-    Test Page for PAM Auth
-    </div>
-    </body>
-    </html>" | sudo tee "$HTML_FILE"
+    sudo systemctl reload nginx | tee -a $LOGFILE
 
     # Set permissions for the web directory
-    sudo chown -R www-data:www-data "$HTML_DIR"
-    sudo chmod -R 755 "$HTML_DIR"
+    sudo chown -R www-data:www-data /var/www/$domain_name | tee -a $LOGFILE
+    sudo chmod -R 755 /var/www/$domain_name | tee -a $LOGFILE
 
-    sudo ln -s /etc/nginx/sites-available/auth_pam /etc/nginx/sites-enabled/
-    sudo systemctl restart nginx
-    echo "PAM authentication enabled."
+    sudo systemctl restart nginx | tee -a $LOGFILE
+    echo "PAM authentication enabled." | tee -a $LOGFILE
+
+    ### Test with curl
+    echo "Testing the virtual host with curl..." | tee -a $LOGFILE
+    read -p "Enter the username : " auth_user
+    read -p "Enter the password : " auth_pass
+    curl -u auth_user:auth_pass http://$domain_name/auth-pam/ | tee -a $LOGFILE
 }   
 
 
@@ -278,7 +279,7 @@ function enable_auth_pam() {
 
 # Options to run the script
 if [ $# -eq 0 ]; then
-    echo "Please choose one of this options: $0 {--install-nginx | --create-virtual-host | --enable-user-dir | --setup-basic-auth | --setup-auth-pam | --remove-nginx}"
+    echo "Please choose one of this options: $0 {--install | --create-virtual-host | --enable-user-dir | --setup-basic-auth | --setup-auth-pam | --remove}" | tee -a $LOGFILE
     exit 1
 fi
 
@@ -304,7 +305,7 @@ case "$1" in
         ;;
 
     *)
-        echo "Usage: $0 {install-nginx | create-virtual-host | enable-user-dir | setup-basic-auth | setup-auth-pam | remove-nginx}"
+        echo "Please choose one of this options: $0 {--install | --create-virtual-host | --enable-user-dir | --setup-basic-auth | --setup-auth-pam | --remove}" | tee -a $LOGFILE
         exit 1
         ;;
 esac
